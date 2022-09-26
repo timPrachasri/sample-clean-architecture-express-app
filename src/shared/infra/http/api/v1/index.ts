60328e9eeeef9144e7b37dc7c9eb27e8d8422f14
timPrachasri <nantawat.prac@gmail.com>
2022-09-27 03:07:52 +0700

    feat: add validation logics for create one item endpoint

--- src/shared/infra/http/api/v1/index.ts
+++ src/shared/infra/http/api/v1/index.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import { body, validationResult } from 'express-validator'
+import isBase64 from 'is-base64'
 
 import { createOneItemHandler } from '~/presentation/http/api/v1'
 import { Result, right } from '~/shared/core'
+import { BadRequest, Result, left, right } from '~/shared/core'
 
 import { BaseHandler } from '../..'
 
@@ -11,8 +14,34 @@ v1Router.get('/healthz', (req, res) => {
   return BaseHandler.parseResponse(res, right(Result.ok()))
 })
 
-v1Router.post('/item', (req, res) => {
-  createOneItemHandler.execute(req, res)
+v1Router.post(
+  '/items',
+  body('name').isLength({ min: 1 }),
+  body('unit.name').isLength({ min: 1 }),
+  body('unit.kernelCount')
+    .isNumeric()
+    .custom((value) => value > 0),
+  body('quantity')
+    .isNumeric()
+    .custom((value) => value > 0),
+  body('location').optional().isLength({ min: 1, max: 20 }),
+  body('note').optional().isLength({ min: 1, max: 20 }),
+  body('picture')
+    .optional()
+    .custom((value) => {
+      return isBase64(value, { mimeRequired: true })
+    }),
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return BaseHandler.parseResponse<null>(
+        res,
+        left(Result.fail<BadRequest>(new BadRequest('Bad Request'))),
+      )
+    }
+    createOneItemHandler.execute(req, res)
+  },
+)
 })
 
 export { v1Router }
